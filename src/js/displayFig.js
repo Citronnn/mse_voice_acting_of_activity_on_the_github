04d75948293d8bbaaa3de0fa9ac36137ff503d69
Citronnn <mrcitron@mail.ru>
2018-10-21 18:03:34 +0300

    div size fixed

--- src/js/displayFig.js
+++ src/js/displayFig.js
@@ -31,6 +31,8 @@ $(document).ready(function(){
         }
     });
     setInterval(function(){
+        if($(window).width()>$('#displaydiv').width())
+            $('#displaydiv').css('min-width',$(window).width()*0.96);
         if($(window).height()>$('#displaydiv').height())
             $('#displaydiv').css('min-height',$(window).height()-55+'px');
     },0);
