562f1dcf0051b44b612b39c83e82f4fee02e1b96
SiarheiZyl <sergzyl99@gmail.com>
2018-12-06 13:59:40 +0300

    Changed funcs for managing cokkie

--- src/js/displayFig.js
+++ src/js/displayFig.js
@@ -212,49 +212,36 @@ function playSound(index, volume) {
     }
 }
 
-
-// возвращает cookie с именем name, если есть, если нет, то undefined
-function getCookie(name) {
-    var matches = document.cookie.match(new RegExp(
-        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-    ));
-    return matches ? decodeURIComponent(matches[1]) : undefined;
+function setCookie(name,value,days = 1) {
+    let expires = '';
+    if (days) {
+        let date = new Date();
+        date.setTime(date.getTime() + (days*24*60*60*1000));
+        expires = '; expires='+ date.toUTCString();
+    }
+    document.cookie = name + '=' + value + expires + '; path=/';
 }
 
-function setCookie(name, value, options) {
-    options = options || {};
-
-    var expires = options.expires;
-
-    if (typeof expires == "number" && expires) {
-        var d = new Date();
-        d.setTime(d.getTime() + expires * 1000);
-        expires = options.expires = d;
-    }
-    if (expires && expires.toUTCString) {
-        options.expires = expires.toUTCString();
-    }
-
-    value = encodeURIComponent(value);
-
-    var updatedCookie = name + "=" + value;
-
-    for (var propName in options) {
-        updatedCookie += "; " + propName;
-        var propValue = options[propName];
-        if (propValue !== true) {
-            updatedCookie += "=" + propValue;
+function getCookie(name) {
+    let nameEQ = name + '=';
+    let ca = document.cookie.split(';');
+    for(let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0)=== ' '){
+            c = c.substring(1, c.length);
+        }
+        if (c.indexOf(nameEQ) === 0) {
+            return c.substring(nameEQ.length, c.length);
         }
     }
-
-    document.cookie = updatedCookie;
+    return null;
 }
 
-function deleteCookie(name) {
-    setCookie(name, "", {
-        expires: -1
-    })
+function  deleteCookie(name) {
+    setCookie(name, '', -1);
 }
+
+
 
 function saveStateInCookies() {
     alert("Prevet")
