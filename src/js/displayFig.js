18243034e29e832c63fb5cffb58edd8a81acf3ff
ierikhon <kinglordlear@gmail.com>
2018-12-07 22:37:46 +0300

    Filter messages for server

--- src/js/displayFig.js
+++ src/js/displayFig.js
@@ -145,7 +145,6 @@ function rands(){
 
 let filter_flags = [];
 function filterChange(id=-1){
-    //TODO Фильтрация по кнопкам, а не по чекбоксам 1
     if(id !== -1) {
         let button = $('#' + id);
         if (button.hasClass('w3-white')) {
@@ -154,26 +153,44 @@ function filterChange(id=-1){
         else {
             button.removeClass('black').addClass('w3-white');
         }
+
+
+        let filter_json = {type: 'filter'};
+        for (let i = 2; i < 12; i++) {
+            if (i !== 4) {
+                let button = $('#' + i);
+                if (button.hasClass('w3-white'))
+                    filter_json[button.val()] = !isLight;
+                else
+                    filter_json[button.val()] = isLight;
+            }
+        }
+        console.log(filter_json);
+        filterChoose(filter_json);
     }
 
-   /* filter_flags=[];
-    let tmp_mass =  $("input:checkbox:checked");
-    for ( let key in tmp_mass){
-        filter_flags.push(tmp_mass[key].value)
-    }
-    if(tmp_mass.length===10 && $('.filterMain')[0].checked === false )
-        $('.filterMain').prop('checked', true);
-    else if (tmp_mass.length!==11)
-        $('.filterMain').prop('checked', false);*/
 }
 
 function use_all_filters_flags() {
-    //TODO Фильтрация по кнопкам, а не по чекбоксам 2
-    /*if($('.filterMain')[0].checked === true)
-        $('.filtercheck').prop('checked', true);
-    else
-        $('.filtercheck').prop('checked', false);
-    filterChange();*/
+    let button = $('.filterMain');
+    if (button.hasClass('w3-white')) {
+        button.removeClass('w3-white').addClass('black');
+    }
+    else {
+        button.removeClass('black').addClass('w3-white');
+    }
+
+    let filter_json = {type:'filter'};
+    for (let i=2; i<12; i++){
+        if (i !== 4) {
+            let button = $('#' + i);
+            filter_json[button.val()] = true;
+        }
+    }
+
+    console.log(filter_json);
+    filterChoose(filter_json);
+
 }
 function add_event(type, jsinfo) {
     let date = new Date();
